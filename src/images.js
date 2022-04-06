6cb1b7b3ca473ffbc87f1e06fdc4ff0b9e11f090
behrad <behrad.hashdari@gmail.com>
2022-04-06 22:16:50 +0430

    lunching

---
+++ src/images.js
@@ -0,0 +1,8 @@
+import image1 from './pc/1.jpg';
+import image2 from './pc/2.jpg';
+import image3 from './pc/3.jpg';
+import image4 from './pc/4.jpg';
+import image5 from './pc/5.jpg';
+import image6 from './pc/6.jpg';
+
+export default[image1,image2,image3,image4,image5,image6]
